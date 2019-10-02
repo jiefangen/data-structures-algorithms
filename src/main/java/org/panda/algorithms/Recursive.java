053864ba2ce051d652032476265234b69dab42f4
jvfagan <1455211171@qq.com>
2019-10-02 19:41:00 +0800

    项目基础配置

---
+++ src/main/java/org/panda/algorithms/Recursive.java
@@ -0,0 +1,10 @@
+package org.panda.algorithms;
+
+/**
+ * 递归算法
+ *
+ * @author jvfagan
+ * @since JDK 1.8  2019/10/2
+ **/
+public class Recursive {
+}
---
+++ src/test/java/org/panda/algorithms/RecursiveTest.java
@@ -0,0 +1,10 @@
+package org.panda.algorithms;
+
+/**
+ * 递归算法单元测试
+ *
+ * @author jvfagan
+ * @since JDK 1.8  2019/10/2
+ **/
+public class RecursiveTest {
+}
